c87eb48e3c0d09c2c84f190ab0580f6a3c912128
ryanjyost <ryost217@gmail.com>
2022-08-30 19:32:22 -0400

    improvements

--- src/App.svelte
+++ src/App.svelte
@@ -15,47 +15,56 @@
 	<h1>Productions</h1>
 
 	<br/>
-	<p>
-		<a style="color: #fff;" href="mailto:jamesredfootproductions@gmail.com">JamesRedfootProductions@gmail.com</a>
-	</p>
-
-	<br/>
-	<br/>
-	<br/>
-
-	<hr/>
-	<br/>
-	<h2>What we're working on.</h2>
-
-	<h4><strong><i>Everybody's got their Demons</i></strong></h4>
-	<p>
-		<i>
-			An animated comedy series about a degenerate who grapples with her religious faith and opts to pursue nunhood to escape the torment of diabolically creative demons who will stop at nothing to make her and everyone else succumb to the dark side of life.
-		</i>
-		<i><u><a href="https://yostandsharp.com/scripts/demons">Click to read the script.</a></u></i>
-	</p>
-
-	<br/>
-	<p>Read our other scripts at <a href="https://yostandsharp.com">yostandsharp.com</a></p>
-	<br/>
 	<hr/>
 
 	<br/>
+	<p>Watch <i>Everybody's Got Their Demons</i> at <a href="https://vimeo.com/736879709/6a1df58d2d">thedemonshow.com</a></p>
+	<p>
+		Email us at <a style="color: #fff;" href="mailto:jamesredfootproductions@gmail.com">JamesRedfootProductions@gmail.com</a>
+	</p>
+
+	<p>Read our scripts at <a href="https://yostandsharp.com">yostandsharp.com</a></p>
+
+
+
+
 	<br/>
 	<br/>
 	<br/>
 
-	<h2>Our team, so far...</h2>
-	<div id="peepsContainer">
-		{#each peeps as peep}
-		<div class="peep">
-			<img src="{`/images/${peep.image}`}" width="100%" height="auto"/>
-			<h4><strong>{peep.name}</strong>, {peep.title}</h4>
-			<br/>
-			<br/>
-		</div>
-		{/each}
-	</div>
+<!--	<hr/>-->
+	<br/>
+<!--	<h2>What we're working on.</h2>-->
+
+<!--	<h4><strong><i>Everybody's got their Demons</i></strong></h4>-->
+<!--	<p>-->
+<!--		<i>-->
+<!--			An animated comedy series about a degenerate who grapples with her religious faith and opts to pursue nunhood to escape the torment of diabolically creative demons who will stop at nothing to make her and everyone else succumb to the dark side of life.-->
+<!--		</i>-->
+<!--		<i><u><a href="https://yostandsharp.com/scripts/demons">Click to read the script.</a></u></i>-->
+<!--	</p>-->
+
+<!--	<br/>-->
+<!--	<p>Read our other scripts at <a href="https://yostandsharp.com">yostandsharp.com</a></p>-->
+<!--	<br/>-->
+<!--	<hr/>-->
+
+<!--	<br/>-->
+<!--	<br/>-->
+<!--	<br/>-->
+<!--	<br/>-->
+
+<!--	<h2>Our team, so far...</h2>-->
+<!--	<div id="peepsContainer">-->
+<!--		{#each peeps as peep}-->
+<!--		<div class="peep">-->
+<!--			<img src="{`/images/${peep.image}`}" width="100%" height="auto"/>-->
+<!--			<h4><strong>{peep.name}</strong>, {peep.title}</h4>-->
+<!--			<br/>-->
+<!--			<br/>-->
+<!--		</div>-->
+<!--		{/each}-->
+<!--	</div>-->
 </main>
 
 <style>
@@ -76,6 +85,7 @@
 		box-shadow: none;
 		outline: none;
 		border-style: solid;
+		width: 100%;
 	}
 
 	img {
@@ -84,11 +94,17 @@
 	}
 
 	main {
-		padding: 1em;
-		padding-top: 100px;
+		/*padding: 1em;*/
+		/*padding-top: 100px;*/
 		width: 100%;
 		max-width: 600px !important;
 		margin: 0 auto;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+		flex-direction: column;
+		text-align: center;
 	}
 
 	h1 {
@@ -99,6 +115,7 @@
 		margin-top: 10px;
 		letter-spacing: 0.03em;
 		font-weight: 700;
+		line-height: 1em;
 	}
 
 	h2 {
@@ -113,6 +130,7 @@
 
 	a {
 		color: #fff;
+		text-decoration: underline;
 	}
 
 	p {
